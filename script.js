b089e6228bef83a352b8aa4839481a9a3836b3d2
Lightning-g944 <sayeeda.riazkasim@gmail.com>
2025-08-25 12:44:11 +0530

    git

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple script for accessible navigation and demo interaction
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for submitting!');
+});
